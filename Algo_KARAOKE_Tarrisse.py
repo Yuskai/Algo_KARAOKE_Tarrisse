6ef6009aea94de1a997c58465d34038dad2d4860
Tarrisse.Allan <93705557+Yuskai@users.noreply.github.com>
2022-01-27 09:58:23 +0100

    Add files via upload

---
+++ Algo_KARAOKE_Tarrisse.py
@@ -0,0 +1,27 @@
+import typing_extensions
+
+
+class playeur 
+def ___init___(self,musique,parole,text,listeLiens)
+self.note = pointGagne
+self.note = notePerdus
+self.text =texte 
+self.pv = pvPerdus
+self.listeLien = listeLiens
+self.score = scoremusique
+self.musique = musiqueChoisit 
+def getText(self)
+return self.text
+def getNoteperdus( self)
+return self.note 
+def getPvPerdus( self):
+return self.pv
+
+class karaoke: 
+def  init selfMusiquechoisit 
+
+musique 1: "Paroles de la chanson Pokemon par Génériques séries télé français"
+musique 2: "Parole du générique de Beyblade : Metal Fusion"
+musique 3: "Petit poney"
+musique 4: "Pomme de reinette et pomme d'api"
+musique 5: "La legende de binks sake - one piece "
